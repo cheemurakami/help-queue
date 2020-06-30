6b0cf432dce1184d565f46dd073fe62eb36d0197
Chisato Murakami <kiwipom1031@gmail.com>
2020-06-29 21:25:03 -0700

    add button to switch display

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -9,22 +9,32 @@ class TicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this); //new code here
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
     let currentlyVisibleState = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List"; // new code
     } else {
       currentlyVisibleState = <TicketList />
-    }
+      buttonText = "Add Ticket"; // new code    }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button> { /* new code */ }
       </React.Fragment>
     );
   }
-
 }
 
+}
 export default TicketControl;
